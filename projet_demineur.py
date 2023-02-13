dbb5d55a1fb6df7b40896641e44f7945c5b251fe
CookieG77 <mcordonn9277@gmail.com>
2023-02-13 14:54:39 +0100

    plateaux rectangles

--- projet_demineur.py
+++ projet_demineur.py
@@ -10,24 +10,25 @@ class MineSweeper:
     """
     Class to play MineSweeper
     """
-    def __init__(self,taille: list[int, int],nbmine: int) -> None:
+    def __init__(self,taille: list[int, int],nbmine: int = 0) -> None:
         """
         Lancement et création du plateau
         """
+        if nbmine == 0:
+            nbmine = int(taille[0] * taille[1] * 0.16)
         self.grille = []
         self.dims = taille
-        for _ in range(taille[1]):
+        for _ in range(taille[0]):
             ltemp = []
-            for _ in range(taille[0]):
+            for _ in range(taille[1]):
                 #Format d'une case : [Type de case, cacher?, drapeau?, nb mines autour]
                 ltemp.append(["", False, False, 0])
             self.grille.append(ltemp)
         nbmineplacer = 0
         while nbmineplacer < nbmine:
-            nbgen = (randint(0, taille[1]-1), randint(0, taille[0]-1))
+            nbgen = (randint(0, taille[0]-1), randint(0, taille[1]-1))
             if self.grille[nbgen[0]][nbgen[1]][0] != "X":
                 self.grille[nbgen[0]][nbgen[1]] = ["X", False, False, 0]
-                print(nbgen)
                 if nbgen[0] != (self.dims[0]-1): #Vérif S
                     self.grille[nbgen[0]+1][nbgen[1]][3] +=1
                 if nbgen[0] != 0: #Vérif N
@@ -52,7 +53,7 @@ class MineSweeper:
         """
         for dim_y in range(self.dims[0]):
             string = "|"
-            print("*---"*self.dims[0] + "*")
+            print("*---"*self.dims[1] + "*")
             for dim_x in range(self.dims[1]):
                 if self.grille[dim_y][dim_x][0] == "":
                     if self.grille[dim_y][dim_x][3] != 0: #Case vide mais mine à coté
@@ -61,7 +62,7 @@ class MineSweeper:
                 else: string += " X " #Case avec mine
                 string += "|"
             print(string)
-        print("*---"*self.dims[0] + "*")
+        print("*---"*self.dims[1] + "*")
 
     def check_win(self) -> bool:
         """
@@ -78,46 +79,37 @@ class MineSweeper:
         """
         Fonction récursive pour révéler les cases.
         """
+        def suite(self, coords: list[int, int], difcoo: list[int, int]) -> None:
+            """
+            Si la case au coordonnées donner est proche d'une mine on l'affiche juste,
+            mais si elle est juste vide et pas proche d'une mine on affiche encore autour.
+            """
+            if self.grille[coords[0]+difcoo[0]][coords[1]+difcoo[1]][1] is True:
+                if (self.grille[coords[0]+difcoo[0]][coords[1]+difcoo[1]][0] == ""
+                    and self.grille[coords[0]+difcoo[0]][coords[1]+difcoo[1]][3]) > 0:
+                    self.reveal_case([coords[0] + difcoo[0], coords[1] + difcoo[1]])
+                elif self.grille[coords[0]+difcoo[0]][coords[1]+difcoo[1]][0] == "":
+                    self.grille[coords[0]+difcoo[0]][coords[1]+difcoo[1]][1] = True
         if coords[0] != (self.dims[0]-1): #Vérif S
-            if self.grille[coords[0]+1][coords[1]][0] == "" and self.grille[coords[0]+1][coords[1]][3] > 0:
-                pass
-            elif self.grille[coords[0]+1][coords[1]][0] == "":
-                pass
+            suite(self, coords, [1,0])
         if coords[0] != 0: #Vérif N
-            if self.grille[coords[0]-1][coords[1]][0] == "" and self.grille[coords[0]-1][coords[1]][3] > 0:
-                pass
-            elif self.grille[coords[0]-1][coords[1]][0] == "":
-                pass
+            suite(self, coords, [-1,0])
         if coords[1] != (self.dims[1]-1): #Vérif E
-            if self.grille[coords[0]][coords[1]+1][0] == "" and self.grille[coords[0]][coords[1]+1][3] > 0:
-                pass
-            elif self.grille[coords[0]][coords[1]+1][0] == "":
-                pass
+            suite(self, coords, [0,1])
         if coords[1] != 0: #Vérif O
-            if self.grille[coords[0]][coords[1]-1][0] == "" and self.grille[coords[0]][coords[1]-1][3] > 0:
-                pass
-            elif self.grille[coords[0]][coords[1]-1][0] == "":
-                pass
-        if coords[0] != (self.dims[0]-1) and coords[1] != (self.dims[1]-1): #Vérif SE
-            if self.grille[coords[0]+1][coords[1]+1][0] == "" and self.grille[coords[0]+1][coords[1]+1][3] > 0:
-                pass
-            elif self.grille[coords[0]+1][coords[1]+1][0] == "":
-                pass
-        if coords[0] != (self.dims[0]-1) and coords[1] != 0: #Vérif SW
-            if self.grille[coords[0]+1][coords[1]-1] == "" and self.grille[coords[0]+1][coords[1]-1] > 0:
-                pass
-            elif self.grille[coords[0]+1][coords[1]-1] == "":
-                pass
-        if coords[0] != 0 and coords[1] != (self.dims[1]-1): #Vérif NE
-            if self.grille[coords[0]-1][coords[1]+1] == "" and self.grille[coords[0]-1][coords[1]+1] > 0:
-                pass
-            elif self.grille[coords[0]-1][coords[1]+1] == "":
-                pass
-        if coords[0] != 0 and coords[1] != 0: #Vérif NW
-            if self.grille[coords[0]-1][coords[1]-1] == "" and self.grille[coords[0]-1][coords[1]-1] > 0:
-                pass
-            elif self.grille[coords[0]-1][coords[1]-1] == "":
-                pass
+            suite(self, coords, [0,-1])
+        if (coords[0] != (self.dims[0]-1)
+            and coords[1] != (self.dims[1]-1)): #Vérif SE
+            suite(self, coords, [1,1])
+        if (coords[0] != (self.dims[0]-1)
+            and coords[1] != 0): #Vérif SW
+            suite(self, coords, [1,-1])
+        if (coords[0] != 0
+            and coords[1] != (self.dims[1]-1)): #Vérif NE
+            suite(self, coords, [-1,1])
+        if (coords[0] != 0
+            and coords[1] != 0): #Vérif NW
+            suite(self, coords, [-1,-1])
 
-PDJ=MineSweeper((9,9), 5)
+PDJ=MineSweeper((5,7))
 PDJ.show_plate()
